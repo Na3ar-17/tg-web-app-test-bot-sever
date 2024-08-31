5a9e9eccdc72c5aee69cfdee2e8ca0ac90d626ea
Na3ar-17 <gavruk2017@gmail.com>
2024-08-31 15:28:40 +0200

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,47 @@
+import * as dotenv from 'dotenv'
+import TelegramBot, { type Message } from 'node-telegram-bot-api'
+import { IFormDetails } from './types/data.types'
+
+dotenv.config()
+
+const token = process.env.BOT_API_TOKEN
+const bot: TelegramBot = new TelegramBot(token || '', { polling: true })
+const webAppUrl = 'https://tg-web-app-test-bot.vercel.app/'
+
+async function main() {
+	bot.on('message', async (msg: Message) => {
+		const chatId = msg.chat.id
+		const text = msg.text
+
+		if (text === '/start') {
+			await bot.sendMessage(chatId, 'Click to the button, and fill the form', {
+				reply_markup: {
+					keyboard: [
+						[{ text: 'Fill the form', web_app: { url: webAppUrl + '/form' } }],
+					],
+					resize_keyboard: true,
+				},
+			})
+			await bot.sendMessage(chatId, 'See our website', {
+				reply_markup: {
+					inline_keyboard: [
+						[{ text: 'Make an order', web_app: { url: webAppUrl } }],
+					],
+				},
+			})
+		}
+		if (msg.web_app_data?.data) {
+			try {
+				const data: IFormDetails = JSON.parse(msg.web_app_data.data)
+				await bot.sendMessage(chatId, 'Thanks for you details!')
+				await bot.sendMessage(chatId, `Your Name: ${data.name}`)
+				await bot.sendMessage(chatId, `Your Surname: ${data.surname}`)
+				await bot.sendMessage(chatId, `Your Gender: ${data.gender}`)
+			} catch (error) {
+				console.log(error)
+			}
+		}
+	})
+}
+
+main()
---
+++ src/types/data.types.ts
@@ -0,0 +1,10 @@
+export interface IFormDetails {
+	name: string
+	surname: string
+	gender: EnumGender
+}
+
+export enum EnumGender {
+	Male,
+	Female,
+}
